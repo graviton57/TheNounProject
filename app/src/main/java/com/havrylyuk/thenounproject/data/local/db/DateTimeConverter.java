e66ebdd8cc532342cb34bb62ae489983d3ff129f
graviton57 <gavrilyuk.igor@gmail.com>
2017-06-08 18:27:17 +0300

    add joda DateTime type converter

---
+++ app/src/main/java/com/havrylyuk/thenounproject/data/local/db/DateTimeConverter.java
@@ -0,0 +1,22 @@
+package com.havrylyuk.thenounproject.data.local.db;
+
+
+import org.greenrobot.greendao.converter.PropertyConverter;
+import org.joda.time.DateTime;
+
+/**
+ * Created by Igor Havrylyuk on 08.06.2017.
+ */
+
+public class DateTimeConverter implements PropertyConverter<DateTime, Long> {
+
+    @Override
+    public DateTime convertToEntityProperty(Long databaseValue) {
+        return new DateTime(databaseValue);
+    }
+
+    @Override
+    public Long convertToDatabaseValue(DateTime entityProperty) {
+        return entityProperty.getMillis();
+    }
+}
